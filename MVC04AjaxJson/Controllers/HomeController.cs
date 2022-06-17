ae41735d0946918057d3439a8a72b744815c461d
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-06-16 21:10:55 -0500

    actualizaciones

--- MVC04AjaxJson/Controllers/HomeController.cs
+++ MVC04AjaxJson/Controllers/HomeController.cs
@@ -88,6 +88,59 @@ namespace MVC04AjaxJson.Controllers
             return Json(cliente, JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult Demo04()
+        {
+            Telefonos t1 = new Telefonos();
+            Telefonos t2 = new Telefonos();
+            Telefonos t3 = new Telefonos();
+            Telefonos t4 = new Telefonos();
+            Telefonos t5 = new Telefonos();
+            Telefonos t6 = new Telefonos();
+
+            t1.numero = "999627956";
+            t1.costo = 700;
+            t1.activo = true;
+
+            t2.numero = "988627956";
+            t2.costo = 800;
+            t2.activo = true;
+
+            t3.numero = "785627956";
+            t3.costo = 750;
+            t3.activo = false;
+
+            t4.numero = "111111111";
+            t4.costo = 1000;
+            t4.activo = false;
+
+            t5.numero = "222222222";
+            t5.costo = 750;
+            t5.activo = true;
+
+            t6.numero = "333333333";
+            t6.costo = 1200;
+            t6.activo = true;
+
+            Clientes cliente1 = new Clientes();
+            cliente1.nombre = "Alicia Neruda";
+            cliente1.telefonos = new Telefonos[] { t1, t2};
+
+            Clientes cliente2 = new Clientes();
+            cliente2.nombre = "Marcos Perez";
+            cliente2.telefonos = new Telefonos[] { t3};
+
+            Clientes cliente3 = new Clientes();
+            cliente3.nombre = "Cesar Delgado";
+            cliente3.telefonos = new Telefonos[] { t4, t5, t6};
+
+            List<Clientes> list = new List<Clientes>();
+            list.Add(cliente1);
+            list.Add(cliente2);
+            list.Add(cliente3);
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
         private class Clientes
         {
             public string nombre { get; set; }
--- MVC04AjaxJson/js/main.js
+++ MVC04AjaxJson/js/main.js
@@ -40,12 +40,54 @@ function demo3() {
         url: "Home/Demo03",
         dataType: "json",
         success: function (result) {
+            //{ "nombre": "Alicia Neruda", 
+            //    "telefonos": [
+            //{
+            //"numero": "999627956",
+            //    "costo": 700,
+            //        "activo": true
+            //},
+            //{
+            //    "numero": "988627956",
+            //        "costo": 800,
+            //            "activo": true
+            //},
+            //{
+            //    "numero": "785627956",
+            //        "costo": 750,
+            //            "activo": false
+            //}
+            //]
+        
             //alert(JSON.stringify(result));
 
             var msg = "Telefonos de " + result.nombre + "<br><span style='font-size: 14px; display: block'>";
 
             for (var i = 0; i < result.telefonos.length; i++) {
                 msg += result.telefonos[i].numero + "(precio: " + result.telefonos[i].costo + (result.telefonos[i].activo ? " y esta activo" : ", no esta activo") + ")<br>";
+            }
+
+            msg += "</span>";
+
+            $("#dlg_message_msg").html(msg);
+            $("#dlg_message").modal("show");
+        }
+    })
+}
+
+function demo4() {
+    $.ajax({
+        url: "Home/Demo04",
+        dataType: "json",
+        success: function (result) {
+
+            var msg = "";
+
+            for (var i = 0; i < result.length; i++) {
+                msg += "Telefonos de " + result[i].nombre + "<br><span style='font-size: 14px; display: block'>";
+                for (var j = 0; j < result[i].telefonos.length; j++) {
+                    msg += result[i].telefonos[j].numero + "(precio: " + result[i].telefonos[j].costo + (result[i].telefonos[j].activo ? " y esta activo" : ", no esta activo") + ")<br>";
+                }
             }
 
             msg += "</span>";
